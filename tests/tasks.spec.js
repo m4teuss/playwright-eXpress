6d91af6296db95144b0af9c8405a383c2ad1cba9
M4teuss <mateuus.s.silva@gmail.com>
2024-09-04 22:53:59 -0300

    interagindo com o browser atraves do mapeamento via expressão regular

--- tests/tasks.spec.js
+++ tests/tasks.spec.js
@@ -1,10 +1,9 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test('deve poder cadastrar uma nova tarefa', async ({page}) => {
+test('deve poder cadastrar uma nova tarefa', async ({ page }) => {
 
-   await page.goto('http://192.168.0.43:8080/')
-  
-   // elemento via id (#id)
-   await page.fill('#newTask', 'Ler um livro de JS')
+    await page.goto('http://192.168.0.43:8080/')
 
+    // mapeando elemento pela propriedade class usando expressão regular
+    await page.fill('input[class*="InputNewTask"]', 'Ler um livro de JS')
 })

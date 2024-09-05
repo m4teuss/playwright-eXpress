d25901af2837d34c6cb6e1efd950f8949f5c8df9
Mateus Silva <mateus.silva@icarotech.com>
2024-09-04 22:15:49 -0300

    adicionado step para validar titulo e wait para execução via browser

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -3,5 +3,11 @@ import {test, expect} from '@playwright/test'
 // declaração assincrona para usar o await
 test('webapp deve estar online', async ({page}) => {
     await page.goto('http://192.168.0.43:8080/')
+
+    // validando titulo
+    await expect(page).toHaveTitle('Gerencie suas tarefas com Mark L')
+
+    // esperando 3s
+    await page.waitForTimeout(3000)
 })
 
